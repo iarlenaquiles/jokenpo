e002cbb13c820af2a7c80a1cdc14b4c09a44f19e
Iarlen <iarlensilva775@gmail.com>
2017-12-19 10:59:46 -0300

    Criando a lógica do jokenpo do usuário

--- index.android.js
+++ index.android.js
@@ -7,33 +7,28 @@ import {
   Button
 } from 'react-native';
 
-class MeuComponente extends Component{
-  render(){
-    return(
-      <View>
-        <Text>{ this.props.teste }</Text>
-      </View>
-    );
-  }
-}
-
 class jokenpo extends Component {
 
   constructor(props){
     super(props);
 
-    this.state = { texto : 'Texto teste 2'};
+    this.state = {escolhaUsuario: ''};
   }
 
-  alteraTexto(){
-    this.setState({ texto: 'Outra coisa'});
+  jokenpo(escolhaUsuario){
+    this.setState({ escolhaUsuario: escolhaUsuario });
   }
 
   render(){
     return(
       <View>
-        <MeuComponente teste={ this.state.texto }></MeuComponente>
-        <Button title='Botão' onPress={() => { this.alteraTexto()} } />
+        <Text>Escolha do Computador</Text>
+        <Text>Escolha do usuário: { this.state.escolhaUsuario }</Text>
+        <Text>Resultado</Text>
+
+        <Button title="pedra" onPress={ () => { this.jokenpo('pedra') } } />
+        <Button title="papel" onPress={ () => { this.jokenpo('papel') } } />
+        <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura') } } />
       </View>
     );
   } 
