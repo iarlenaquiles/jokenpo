7d3e0757138c70cfb079882fd3bf997a727aa471
Iarlen <iarlensilva775@gmail.com>
2017-12-20 16:47:08 -0300

    Ajustando o tamanho do topo

--- src/components/topo.js
+++ src/components/topo.js
@@ -10,7 +10,7 @@ class Topo extends Component {
   render() {
     return ( 
       <View>
-        <Image source={imagem} style={{width: 500}} />
+        <Image source={imagem} style={{width: 430}} />
       </View>
     );
   }
